ba00fb28a6db320b5f9cdd8cd73569338f154033
Makar <154985504+Sayflagdog@users.noreply.github.com>
2025-11-10 17:33:43 +0300

    Create lab2_part1_S3.sql

---
+++ lab2/main files sql/lab2_part1_S3.sql
@@ -0,0 +1,13 @@
+--Часть1
+
+--S3
+SELECT u.username,
+       COUNT(lh.event_id)       AS plays,
+       COALESCE(SUM(lh.progress_seconds), 0) AS seconds_total
+FROM public.users u
+LEFT JOIN public.listening_history lh
+       ON lh.user_id = u.user_id
+      AND (lh.listened_at)::timestamp >= now() - INTERVAL '7 days'
+GROUP BY u.user_id, u.username
+ORDER BY plays DESC, seconds_total DESC
+LIMIT 10;
