7d2934c1285ecee7b5ade17004343f5efca68b27
Makar <154985504+Sayflagdog@users.noreply.github.com>
2025-11-10 17:34:10 +0300

    Add SQL query to retrieve playlist details

---
+++ lab2/main files sql/lab2_part1_S4.sql
@@ -0,0 +1,22 @@
+--Часть1
+
+--S4
+SELECT p.playlist_id,
+       p.title,
+       u.username AS owner,
+       (SELECT COUNT(*)
+          FROM public.track_playlist tp
+         WHERE tp.playlist_id = p.playlist_id) AS tracks_in_playlist,
+       p.is_public, p.created_at
+FROM public.playlists p
+JOIN public.users u ON u.user_id = p.owner_id
+ORDER BY tracks_in_playlist DESC, p.created_at DESC;
+
+
+
+
+
+
+
+
+
